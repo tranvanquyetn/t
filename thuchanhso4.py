39954bf299ff86b511548aea7b8d433887175ce9
tranvanquyetn <48422559+tranvanquyetn@users.noreply.github.com>
2019-04-04 17:05:51 +0700

    Add files via upload

---
+++ thuchanhso4.py
@@ -0,0 +1,25 @@
+#nhap,them, tim kiem,sap xep, cat phan tu trong lisst
+list=['a','b','c','d','mno']
+print(list)
+
+list.append('e')
+print(list)
+
+list.remove('e')
+print(list)
+
+list.index('mno')
+print(list.index('mno'))
+
+list.sort(reverse=True)
+print(list)
+
+list.sort(reverse=False)
+print(list)
+
+list.remove('a')
+print(list)
+list.remove('mno')
+print(list)
+
+
