f87bde19f6135fd3a214df454c14554f11c320e5
tranvanquyetn <48422559+tranvanquyetn@users.noreply.github.com>
2019-04-08 08:08:31 +0700

    Add files via upload

---
+++ list so nguyen.py
@@ -0,0 +1,15 @@
+lista =[1,2,3,4,5]
+print(lista)
+listb =[6,7,8,3,5]
+print(listb)
+listc=lista+listb
+print(listc)
+listc.sort(reverse=False)
+print(listc)
+listc=['x']
+print(listc.count('x'))
+# tạo list số nguyên ngẫu nhiên
+
+
+
+
