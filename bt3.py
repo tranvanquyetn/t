60b73b88fdd789965d3da000210e37deb7dee21e
tranvanquyetn <48422559+tranvanquyetn@users.noreply.github.com>
2019-04-09 00:03:58 +0700

    Add files via upload

---
+++ bt3.py
@@ -0,0 +1,9 @@
+data={'a':1,'b':2,'c':3^2}
+print(data['b'])
+print(data['c'])
+
+
+#định nghĩa một hàm có thể in dictionary chứa các key là số
+#từ 1 đến 20 (bao gồm cả 1 và 20) và các giá trị bình phương
+#của chúng
+
