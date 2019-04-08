07b7b1e96a02ef7a52ea688a8dbe43abc1384ea3
tranvanquyetn <48422559+tranvanquyetn@users.noreply.github.com>
2019-04-08 23:20:03 +0700

    Add files via upload

---
+++ bt1.py
@@ -0,0 +1,9 @@
+#Viết chương trình chấp nhận một chuỗi số, phân tách bằng
+#dấu phẩy từ giao diện điều khiển, tạo ra một danh sách và một
+#tuple chứa mọi số
+tup1=(1,222,333,4,56,7,8,7,75,4)
+tup2=(6,77777,3,42,1,35,7,8,7,5)
+print(tup1)
+print(tup2)
+tup3=tup1+tup2
+print(tup3)
