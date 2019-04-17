4790b90a53f2466f32508a962a4227d58efe1a71
tranvanquyetn <48422559+tranvanquyetn@users.noreply.github.com>
2019-04-17 21:45:45 +0700

    Add files via upload

---
+++ th1.8.1.py
@@ -0,0 +1,15 @@
+import math as kc
+x1=int(input("nhap x1:"))
+y1=int(input("nhap y2:"))
+
+x2=int(input("nhap x2:"))
+y2=int(input("nhap y2:"))
+
+a1=(x2-x1)*(x2-x1)
+a2=(y2-y1)*(y2-y1)
+
+m=kc.sqrt(a1+a2)
+print('khoang cach la:',m)
+# nhập hai điểm và tính khoảng cách
+
+
